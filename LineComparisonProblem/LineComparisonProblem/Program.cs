4af7eb09a13b97670d00d4ff0153909856f45133
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-04 10:43:34 +0530

    [Rohan]Add:checking if lines are equal or wich line is greater

--- LineComparisonProblem/LineComparisonProblem/Program.cs
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +14,24 @@ namespace LineComparisonProblem
             CalculateLength line_One = new CalculateLength(1,2,3,4);
             double length_One = line_One.Calculate();
 
-            CalculateLength line_two = new CalculateLength(5,6,7,6);
+            CalculateLength line_two = new CalculateLength(5,6,7,9);
             double length_two = line_two.Calculate();
 
             if (length_One.Equals(length_two))
                 Console.WriteLine("Lines are equal");
             else
                 Console.WriteLine("Lines are not equal");
+
+            //UC3
+            int a = length_One.CompareTo(length_two);
+            Console.WriteLine(a);
+            if (a == 0)
+                Console.WriteLine("Both Lines are equal");
+            else if (a > 0)
+                Console.WriteLine("Line one is greater");
+            else
+                Console.WriteLine("Line two is greater");
+
             Console.ReadKey();
         }
     }
