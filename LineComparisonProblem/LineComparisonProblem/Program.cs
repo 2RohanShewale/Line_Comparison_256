cc3c10387f51173c66ad26804b2eb3c8ea4d2bff
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-04 10:30:46 +0530

    [Rohan]Add:Creating line

---
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateLength length_One = new CalculateLength(1,2,3,4);
+            length_One.Calculate();
+
+
+            Console.ReadKey();
+        }
+    }
+}
